7cc1a66b002e743602b0d00a11ef78942d2e219b
allrounder745 <princekumarjha745@gmail.com>
2025-07-20 10:05:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT_URL="https://wex55.net/SPORTS_FOX_CRICKET/index.m3u8?token=226b90053b920b7f906879e23cc6c1c7"
+INPUT_URL="https://dai.google.com/ssai/event/nmQFuHURTYGQBNdUG-2Qdw/master.m3u8"
 RTMP_URL="rtmps://mumbai.onestream.studio:19350/live/live_4139402_v5upz05e3?auth=p_auth_4139402_03wjzhl3u"
 
 echo "Starting restream from $INPUT_URL to $RTMP_URL"
